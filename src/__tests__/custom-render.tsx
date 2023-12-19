84489ec184f924018fc425110f05421218f13175
yuichiyasui <yassan.b.a.w@gmail.com>
2023-12-19 21:15:58 +0900

    Shorten test duration

--- src/__tests__/custom-render.tsx
+++ src/__tests__/custom-render.tsx
@@ -1,7 +1,6 @@
 import userEvent from "@testing-library/user-event";
 import { render } from "@testing-library/react";
 import { Providers } from "@/app/providers";
-import RootLayout from "@/app/layout";
 
 export const renderWithProviders = (ui: React.ReactElement) => {
   const user = userEvent.setup();
